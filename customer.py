91b25f210ca777d9d915130ffd9cbd0ceb8719c3
phongamnart <pknart17@gmail.com>
2023-10-01 09:52:29 +0700

    admin page

--- customer.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow
-
-class CustomerWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Customer Window')
-        self.setGeometry(200, 200, 400, 200)
